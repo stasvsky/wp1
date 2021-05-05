1cf41d2da7febbe5e0caf26b593196ea404a1caa
Stanislav Vasilevskiy <s.vasilewskiy@gmail.com>
2021-05-05 10:56:16 +0300

    Add child theme

---
+++ wp-content/themes/my_theme/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="UTF-8" />
+	<?php wp_head(); ?>
+	<title><?php wp_title(' - ', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+</head>
+<body>
+
+<?php
+	$args = array(
+		'theme_location' => 'Header',
+		'menu' => 'Main',
+		'container' => 'nav',
+		'container_class' => 'nav',
+		'items_wrap' => '<ul>%3$s</ul>'
+	);
+
+	wp_nav_menu($args);
+?>
