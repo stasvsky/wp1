e3396d9d747b486ca03c83aa69bc9f01cfdd5a4f
Stanislav Vasilevskiy <s.vasilewskiy@gmail.com>
2021-05-07 11:17:32 +0300

    Removed unnecessary conditions

--- wp-content/themes/twentytwentyone-child/single-book.php
+++ wp-content/themes/twentytwentyone-child/single-book.php
@@ -2,15 +2,11 @@
 
 <h1><?php single_post_title(); ?></h1>
 
-<?php if (get_post_type() == 'book') {?>
-
-	<ul class="books_fields">
-		<li>Author: <?= get_field('bk_book_author') ?></li>
-		<li>Genre: <?= get_field('bk_genre') ?></li>
-		<li>The year of publishing: <?= get_field('bk_the_year_of_publishing') ?></li>
-		<li>Number of pages: <?= get_field('bk_number_of_pages') ?></li>
-	</ul>
-
-<?php }?>
+<ul class="books_fields">
+	<li>Author: <?= get_field('bk_book_author') ?></li>
+	<li>Genre: <?= get_field('bk_genre') ?></li>
+	<li>The year of publishing: <?= get_field('bk_the_year_of_publishing') ?></li>
+	<li>Number of pages: <?= get_field('bk_number_of_pages') ?></li>
+</ul>
 
 <?php get_footer(); ?>
--- wp-content/themes/twentytwentyone-child/single-journal.php
+++ wp-content/themes/twentytwentyone-child/single-journal.php
@@ -2,15 +2,11 @@
 
 <h1><?php single_post_title(); ?></h1>
 
-<?php if (get_post_type() == 'journal') {?>
-
-	<ul class="journal_fields">
-		<li>Author: <?= get_field('jl_book_author') ?></li>
-		<li>Genre: <?= get_field('jl_genre') ?></li>
-		<li>The year of publishing: <?= get_field('jl_the_year_of_publishing') ?></li>
-		<li>Number of pages: <?= get_field('jl_number_of_pages') ?></li>
-	</ul>
-
-<?php }?>
+<ul class="journal_fields">
+	<li>Author: <?= get_field('jl_book_author') ?></li>
+	<li>Genre: <?= get_field('jl_genre') ?></li>
+	<li>The year of publishing: <?= get_field('jl_the_year_of_publishing') ?></li>
+	<li>Number of pages: <?= get_field('jl_number_of_pages') ?></li>
+</ul>
 
 <?php get_footer(); ?>
